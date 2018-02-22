fdcaebfad18fe3cb905ea3b68d22d85770d7e088
JCToll <32552531+JCToll@users.noreply.github.com>
2018-02-21 21:26:31 -0800

    updated the confirm message

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function capture() {
 event.preventDefault()
 	var theEmail= document.custInfo.userEmail.value
 	console.log("Thank you so much for signing up for our mailing list, " + theEmail )
-	confirm(" Thank you for signing up for our mailing list!")
+	confirm(" Thank you for signing up for our mailing list!  \n This also writes to the console")
 }
 
 
